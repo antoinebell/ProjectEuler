6c03169e492eb7cf6fd5bd15f87fad274bfbc76d
Antoine Bellanger <dev@antoinebellanger.ch>
2019-02-21 21:47:41 +0100

    7.

---
+++ 7. 10001st prime.py
@@ -0,0 +1,19 @@
+import math1
+
+def eratosthenes(n):
+    multiples = []
+    primes = []
+    for i in range(2, n + 1):
+        if i not in multiples:
+            primes.append(i)
+            if len(primes) == 10001:
+                return primes
+            else:
+                print(len(primes))
+            for j in range(i * i, n + 1, i):
+                multiples.append(j)
+
+    return primes
+
+if __name__ == "__main__":
+    print(eratosthenes(150000))
